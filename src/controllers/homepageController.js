122ee427b73914102c80bb1e996e7d7e32446968
garino11 <garijero@gmail.com>
2022-03-28 15:25:07 +0200

    ds

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -79,7 +79,7 @@ let handleMessage = (sender_psid, received_message) => {
     let response;
 
     // Checks if the message contains text
-    if (received_message.text) {
+    if (sender_psid) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         response = {
