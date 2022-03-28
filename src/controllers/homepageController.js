4a4e9d9977ee2895da8884329cf82f170b0e9715
garino11 <garijero@gmail.com>
2022-03-28 15:15:56 +0200

    ds

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -79,11 +79,11 @@ let handleMessage = (sender_psid, received_message) => {
     let response;
 
     // Checks if the message contains text
-    if (received_message.text) {
+    if (sender_psid) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         response = {
-            "text": `Vous avez envoyer "${sender_psid.text}" un message vers Heroku: "${received_message.text}". Now send me an attachment!`
+            "text": `Vous avez envoyer "${sender_psid.id}" un message vers Heroku: "${received_message.text}". Now send me an attachment!`
         }
     } else if (received_message.attachments) {
         // Get the URL of the message attachment
